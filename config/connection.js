3b0b85c1939406ad8339ebb59fafb87b299b5275
Rivenception <slicklootz@gmail.com>
2020-11-09 21:10:42 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Redsox88*",
+  password: "password",
   database: "time_monitorDB"
 });
 
