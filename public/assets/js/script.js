a118a4c4293d67f7c57d7cae9cdac661915c578c
Rivenception <ricky.mingione@gmail.com>
2020-11-09 22:07:14 -0500

    fixed the script.js for dynamic creation of the dropdowns for each department

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     let task = {
         pm_tasks: ["Kick-Off", "Project Planning", "Design Meeting", "Program Tracking", "Customer Support", "Program (Meeting)"],
         mfg_tasks: ["Tooling-LP", "Tooling-Mold", "Tooling-Hotwire", "Tooling Programming", "GPD cushion rev", "GPD dress covers", "Admin"],
-        eng_tasks: ["Drawings",],
+        eng_tasks: ["Drawings"],
         admin_tasks: ["Training", "Out of Office", "Work Share", "Furlough"],
     }
 
@@ -14,52 +14,57 @@ $(document).ready(function () {
     }
     const minutes = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510]
 
-    let options = $("<option>");
-
     function categories() {
-        console.log("this works");
         if (window.location.href === "http://localhost:8080/eng") {
-            for (let i = 0; i < category.eng_category; i++) {
-                let dropdown = options.attr("value: " + category.eng_category[i]).text(category.eng_category[i]);
+            console.log("eng page");
+            for (let i = 0; i < category.eng_category.length; i++) {
+                console.log(category.eng_category[i]);
+                let dropdown = $("<option>").attr("value", category.eng_category[i]).text(category.eng_category[i]);
+                // dropdown = dropdown.text(category.eng_category[i]);
                 $("#inputGroupCategory").append(dropdown);
             }
         } else if (window.location.href === "http://localhost:8080/mfg") {
-            for (let i = 0; i < category.mfg_category; i++) {
-                let dropdown = options.attr("value: " + category.mfg_category[i]).text(category.mfg_category[i]);
+            console.log("mfg page");
+            for (let i = 0; i < category.mfg_category.length; i++) {
+                let dropdown = $("<option>").attr("value", category.mfg_category[i]).text(category.mfg_category[i]);
                 $("#inputGroupCategory").append(dropdown);
             }
         } else if (window.location.href === "http://localhost:8080/pm") {
-            for (let i = 0; i < category.pm_category; i++) {
-                let dropdown = options.attr("value: " + category.pm_category[i]).text(category.pm_category[i]);
+            console.log("pm page");
+            for (let i = 0; i < category.pm_category.length; i++) {
+                let dropdown = $("<option>").attr("value", category.pm_category[i]).text(category.pm_category[i]);
                 $("#inputGroupCategory").append(dropdown);
             }
         }
     };
 
     function tasks() {
-        console.log("this works");
+        console.log(task.eng_tasks);
         if (window.location.href === "http://localhost:8080/eng") {
-            for (let i = 0; i < task.eng_tasks; i++) {
-                let dropdown = options.attr("value: " + task.eng_tasks[i]).text(task.eng_tasks[i]);
+            console.log("eng page");
+            for (let i = 0; i < task.eng_tasks.length; i++) {
+                let dropdown = $("<option>").attr("value", task.eng_tasks[i]).text(task.eng_tasks[i]);
                 $("#inputGroupTask").append(dropdown);
             }
         } else if (window.location.href === "http://localhost:8080/mfg") {
-            for (let i = 0; i < task.mfg_tasks; i++) {
-                let dropdown = options.attr("value: " + task.mfg_tasks[i]).text(task.mfg_tasks[i]);
+            console.log("mfg page");
+            for (let i = 0; i < task.mfg_tasks.length; i++) {
+                let dropdown = $("<option>").attr("value", task.mfg_tasks[i]).text(task.mfg_tasks[i]);
                 $("#inputGroupTask").append(dropdown);
             }
         } else if (window.location.href === "http://localhost:8080/pm") {
-            for (let i = 0; i < task.pm_tasks; i++) {
-                let dropdown = options.attr("value: " + task.pm_tasks[i]).text(task.pm_tasks[i]);
+            console.log("pm page");
+            for (let i = 0; i < task.pm_tasks.length; i++) {
+                let dropdown = $("<option>").attr("value", task.pm_tasks[i]).text(task.pm_tasks[i]);
                 $("#inputGroupTask").append(dropdown);
             }
         }
     }
 
     function times() {
-        console.log("this works");
-        for (let i = 0; i < minutes; i++) {
-            let dropdown = options.attr("value: " + minutes[i]).text(minutes[i]);
+        console.log("timer");
+        for (let i = 0; i < minutes.length; i++) {
+            let dropdown = $("<option>").attr("value", minutes[i]).text(minutes[i]);
             $("#inputGroupTime").append(dropdown);
         }
     }
